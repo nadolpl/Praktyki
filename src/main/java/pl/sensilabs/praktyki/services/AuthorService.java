cacaad5e8540046deafdfb7218658ec16d46fadf
nadolpl <nadolgaming@gmail.com>
2025-03-03 15:20:38 +0100

    AuthorController, AuthorService PublisherService

---
+++ src/main/java/pl/sensilabs/praktyki/services/AuthorService.java
@@ -0,0 +1,39 @@
+package pl.sensilabs.praktyki.services;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import pl.sensilabs.praktyki.mappers.AuthorMapper;
+import pl.sensilabs.praktyki.repositories.AuthorRepository;
+import pl.sensilabs.praktyki.responses.AuthorResponse;
+
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class AuthorService {
+
+    private final AuthorRepository authorRepository;
+
+    public Iterable<AuthorResponse> findAll() {
+        return authorRepository.findAll()
+                .stream()
+                .map(AuthorMapper::toResponse)
+                .collect(Collectors.toSet());
+    }
+
+    public AuthorResponse findById(UUID authorId) {
+        return authorRepository.findById(authorId)
+                .map(AuthorMapper::toResponse)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+
+    public void deleteById(UUID authorId) {
+        if (authorRepository.existsById(authorId)) {
+            authorRepository.deleteById(authorId);
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+}
