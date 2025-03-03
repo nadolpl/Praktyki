0d144b3a7b4f69252a4c6578562fec65cd3eeca2
nadolpl <nadolgaming@gmail.com>
2025-03-03 11:58:43 +0100

    init

---
+++ src/main/java/pl/sensilabs/praktyki/PraktykiApplication.java
@@ -0,0 +1,13 @@
+package pl.sensilabs.praktyki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PraktykiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PraktykiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sensilabs/praktyki/PraktykiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sensilabs.praktyki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PraktykiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
