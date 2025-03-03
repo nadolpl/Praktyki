7c77b1f0c8717df9b158f6cdfd033efd3ae25a0d
AveeSuuu <konrad.niedzielski03@gmail.com>
2025-03-03 13:43:59 +0100

    Implemented relations for Book and PublishedBook.

--- src/main/java/pl/sensilabs/praktyki/entities/PublishedBook.java
+++ src/main/java/pl/sensilabs/praktyki/entities/PublishedBook.java
@@ -1,9 +1,12 @@
 package pl.sensilabs.praktyki.entities;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import java.time.Instant;
 import java.util.UUID;
@@ -28,4 +31,16 @@ public class PublishedBook {
   private Integer bookTypeId;
   private Integer releaseNumber;
   private Instant publishDate;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "book_id")
+  private Book book;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "publisher_id")
+  private Publisher publisher;
+
+  @ManyToOne(cascade = CascadeType.ALL)
+  @JoinColumn(name = "book_type_id")
+  private BookType bookType;
 }
